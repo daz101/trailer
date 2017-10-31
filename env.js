dd19f6ac98f23771a9b722c0061d4c8e392067e7
Daricia Wilkinson <Daricia Wilkinson>
2017-10-31 17:08:10 -0400

    added file

---
+++ env.js
@@ -0,0 +1,3 @@
+process.env.THEMOVIEDB_API_KEY = '16a2aa2c7f485bb865100b4b708018f6'
+process.env.MONGO_URL = 'mongodb://admin:randomfishes234@ds243325.mlab.com:43325/heroku_ln6dp8f9'
+//something added something
