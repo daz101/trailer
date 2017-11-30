ccdbf7e6b2e4381df134b6b224a3fc0b38729210
tejaravi89 <rkandul@g.clemson.edu>
2017-11-30 16:23:43 -0500

    Update script-intro.js
    Added finish method to post first survey answers in database

--- views/js/script-intro.js
+++ views/js/script-intro.js
@@ -6,6 +6,7 @@ var userFromUrl = window.location.pathname;
 var userid = userFromUrl.replace('/', '');
 var consent_check=0; 
 //var nrOfQns = $('.question .rad_row').length;
+var firstanswers = [];
 
 $(document).ready(function() {
 	//welcome
@@ -119,16 +120,15 @@ $( "#Q3 .rad_row td" ).click(function() {
 		var hookpage = $("#verbalpage4");
 		if(isSurveyComplete(hookpage))
 		{
-		  $('#verbalpage3, #verbalpage3button').fadeOut("slow", function() {
-		  $('#verbalpage3, #verbalpage3button').hide();
-		  $('#verbalpage4, #resize_nextOverview').show().fadeIn("slow");
-		   });
+		   finish();
+		   start();
 		}
 	});
 	
 /*
  FUNCTION TO CHECK IF ALL QUESTIONS HAVE BEEN ANSWERED.
  */
+ 
 function isSurveyComplete(hookpage) {
 	var counter=0;
 		for(var i=1; i<=hookpage.find('.question .rad_row').length; i++) 
@@ -140,7 +140,15 @@ function isSurveyComplete(hookpage) {
 			}
         };
 		if(counter==1)return false;
+		else{
+
+		for(var i=1; i<=hookpage.find('.question .rad_row').length; i++) 
+		{
+			firstanswers.push(hookpage.find('input[name=radOpt_'+i+']:checked').val());
+        };	
 		return true;
+		
+	   }
 }
 // ------------------------FIRST SURVEY PAGES END-------------------------------------------------------
 	
@@ -174,6 +182,26 @@ function start() {
     success: function() {
     confirmUnload = false;
     location.reload(true); 	
+    },
+    error: function(err) {
+      console.log(err.responseText);
+    }
+  });
+}
+
+function finish() {
+
+	$.ajax({
+    type: 'POST',
+    url: '/api/update/firstanswers',
+    data: {
+      userid: userid,
+      firstanswers: JSON.stringify(firstanswers)
+    },
+    dataType: 'json',
+    success: function() {
+     //  confirmUnload = false;
+    	 location.reload(true);
     },
     error: function(err) {
       console.log(err.responseText);
